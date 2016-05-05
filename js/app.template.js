b96629d667a0e210b6fdd133db5ea7144747daf2
leasonlive <Administrator@PC201408310406>
2016-05-05 10:29:38 +0800

    楼盘详细页面调通，用json模式，楼盘名称已经替换

--- js/app.template.js
+++ js/app.template.js
@@ -5,7 +5,11 @@
 
 	var template = null;
 	var getTemplate = function(name, header, content) {
+		console.log("加载模板 name:" + name + " header:" + header + " content:" + content);
+		console.log("公用父模板:" + template);
 		if (!template) {
+			console.log("公用父模板不存在，开始创建");
+			console.log("加载模板头:" + header);
 			//预加载共用父模板；
 			var headerWebview = $.preload({
 				url: header,
@@ -17,6 +21,7 @@
 					mType: 'main'
 				}
 			});
+			console.log("加载内容(子模板）:" + content);
 			//预加载共用子webview
 			var subWebview = $.preload({
 				url: !content ? "" : content,
@@ -29,6 +34,7 @@
 					mType: 'sub'
 				}
 			});
+			console.log("给子模板添加事件:loaded,加载完成之后自动显示")
 			subWebview.addEventListener('loaded', function() {
 				setTimeout(function() {
 					subWebview.show();
@@ -52,11 +58,14 @@
 	};
 	// 初始化模版
 	var initTemplates = function() {
+		console.log("初始化模板");
 		getTemplate('default', 'template.html');
 	};
 	// 进入详情界面
 	var toDatail = function(conf) {
 		conf = conf || {};
+		console.log("conf.id:" + conf.id);
+		console.log("conf.href:", conf.href);
 		if (!conf.href) {
 			return false;
 		}
@@ -73,18 +82,30 @@
 		//获得共用子webview
 		var contentWebview = template.content;
 		//通知模板修改标题，并显示隐藏右上角图标；
+		console.log("更新头部信息");
+		console.log("conf.id:" + conf.id);
 		$.fire(headerWebview, 'updateHeader', conf);
+		console.log("conf.id:" + conf.id);
+		console.log("ios:" + $.os.ios + " android:" + $.os.android + " $.os.version:" + $.os.version)
 		if ($.os.ios || ($.os.android && parseFloat($.os.version) < 4.4)) {
+			console.log("ios或者android版本小于4.4");
 			var reload = true;
 			if (!template.loaded) {
-				if (contentWebview.getURL() != this.href) {
-					contentWebview.loadURL(this.href);
+				console.log("模板还没有被加载过,开始加载");
+				console.log("内容模板的url:" + contentWebview.getURL());
+				console.log("this.href:" + this.href);
+				console.log("需要加载的地址:" + conf.id);
+				if (contentWebview.getURL() != conf.id) {
+					console.log("模板地址和需要加载的地址不一致，重新加载新地址");
+					contentWebview.loadURL(conf.id);
 				} else {
 					reload = false;
 				}
 			} else {
+				console.log("模板已经加载数据成功，不在加载")
 				reload = false;
 			}
+			console.log("realod:" + reload);
 			(!reload) && contentWebview.show();
 			headerWebview.show("pop-in", 150);
 		}
